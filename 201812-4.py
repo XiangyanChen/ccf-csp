c2ca1b575358ccb35e35a1a40393dc9d4c07715a
XyChen <13030880@qq.com>
2019-02-22 23:36:48 +0800

    initial 201812-4

---
+++ 201812-4.py
@@ -0,0 +1,14 @@
+class Graph:
+    def __init__(self):
+        vexs = list()
+        arc = list()
+        num_nodes = 0
+        num_edges = 0
+if __name__ == "__main__":
+    n = int(input())
+    m = int(input())
+    root = int(input())
+    for i in range(m):
+        input_list = list(map(int, input().split()))
+        start, end, value = input_list[0], input_list[1], input_list[2]
+    
