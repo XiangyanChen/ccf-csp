443a76efd88e2ff25c9ebaf28a2da8ecf71f573f
xychen <13030880@qq.com>
2019-02-24 17:06:58 +0800

    add 100 score program of 201809-3

--- 201809-3.py
+++ 201809-3.py
@@ -1,3 +1,6 @@
+"""
+    A B C表示不同层级
+"""
 '''
 11 5
 html
@@ -18,8 +21,10 @@ div p
 div div p
 '''
 import re
+import copy
 class Node:
-    def __init__(self, rank, element_name, id_name):
+    def __init__(self, line_id, rank, element_name, id_name):
+        self.line_id = line_id
         self.rank = rank
         self.element_name = element_name
         self.id_name = id_name
@@ -28,8 +33,8 @@ class Node:
 
 if __name__ == "__main__":
     num_list = (list(map(int, input().split())))
-    n = num_list[0]
-    m = num_list[1]
+    n = num_list[0] #文档n行
+    m = num_list[1] #需要检测的m行
     line_list = list()
     selector_list = list()
     node_list = list()
@@ -39,23 +44,45 @@ if __name__ == "__main__":
         dot_str = re.match("^\.*", line).group()
         line = line.replace(dot_str, "")
         try:
-            node = Node((len(dot_str) + 2)/ 2, line.split()[0].lower(), line.split()[1].lower())
+            node = Node(i + 1, (len(dot_str) + 2) / 2, line.split()[0].lower(), line.split()[1])
         except:
-            node = Node((len(dot_str) + 2)/ 2, line.split()[0].lower(), None)
+            node = Node(i + 1, (len(dot_str) + 2) / 2, line.split()[0].lower(), None)
         node_list.append(node)
     for i in range(m):
         selector_list.append(input())
-    for node in node_list:
-        print(node)
-    # for line in line_list:
-    #     print(line)
-    # for selector in selector_list:
-    #     index_list = list()
-    #     for index, line in enumerate(line_list):
-    #         line = line.replace(".", "")
-    #         if selector in line.split(" "):
-    #             index_list.append(index)
-    #     print(len(index_list), end=" ")
-    #     for index in index_list:
-    #         print("%d" % (index + 1), end=" ")
-    #     print()
+    for selector_input in selector_list:
+        line_id_list = list()
+        selector_list = selector_input.split(" ")
+        #元素小写化，ID选择器不管
+        for i in range(len(selector_list)):
+            if ("#" not in selector_list[i]):
+                selector_list[i] = selector_list[i].lower()
+        if (len(selector_list) == 1):
+            for node in node_list:
+                if ("#" in selector_list[0]):
+                    if (selector_list[0] == node.id_name):
+                        line_id_list.append(node.line_id)
+                else:
+                    if (selector_list[0] == node.element_name):
+                        line_id_list.append(node.line_id)
+        else:
+            selector_list.reverse() #旋转，自下向上的找选择题
+            for node in node_list:
+                if (("#" in selector_list[0] and node.id_name == selector_list[0]) or selector_list[0] == node.element_name):   #匹配到最下面的选择器，开始匹配上面的
+                    temp_node_list = copy.deepcopy(node_list[:node_list.index(node)])
+                    pre_rank = node.rank
+                    i = 1
+                    while (i < len(selector_list) and len(temp_node_list) != 0):    #从下自上一个一个匹配过去
+                        temp_node = temp_node_list.pop()
+                        if (("#" in selector_list[i] and temp_node.id_name == selector_list[i]) or selector_list[i] == temp_node.element_name):
+                            if (temp_node.rank < pre_rank):
+                                pre_rank = temp_node.rank
+                                i += 1
+                            else:
+                                break
+                        if (len(temp_node_list) == 0):  #遍历完文档还没匹配到，就是失败了
+                            break
+                    if (i == len(selector_list)):
+                            # if (i == len(selector_split_result)):
+                        line_id_list.append(node.line_id)
+        print(str(len(line_id_list)) + " " + " ".join(map(str, line_id_list)))
